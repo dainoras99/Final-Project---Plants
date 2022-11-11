f4091c76c22d98966d06e175588b870f15f5b0ae
dainoras99 <ziukasdainoras@gmail.com>
2022-11-11 09:37:11 +0200

    changes

--- angular-plants/angular-plants/src/app/app.module.ts
+++ angular-plants/angular-plants/src/app/app.module.ts
@@ -1,39 +1,28 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppComponent } from './app.component';
 import { PlantListComponent } from './components/plant-list/plant-list.component';
-
 import { HttpClientModule } from "@angular/common/http"
 import { ProductService } from './services/product.service';
-
 import { Routes, RouterModule} from '@angular/router';
 import { PlantCategoryMenuComponent } from './components/plant-category-menu/plant-category-menu.component';
 import { SearchComponent } from './components/search/search.component'
-
 import { MatDialogModule } from '@angular/material/dialog';
 import { RegistrationComponent } from './components/registration/registration.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { LoginComponent } from './components/login/login.component';
 import { PlantDetailsComponent } from './components/plant-details/plant-details.component';
 import { CartStatusComponent } from './components/cart-status/cart-status.component';
 import { DrawerComponent } from './components/drawer/drawer.component';
-
-import { MatToolbarModule } from '@angular/material/toolbar'
 import { MatSidenavModule } from '@angular/material/sidenav'
-import { MatIconModule } from '@angular/material/icon'
-import { MatListModule } from '@angular/material/list'
-import { MatButtonModule } from '@angular/material/button';
 import { CartComponent } from './components/cart/cart.component';
 import { CheckoutComponent } from './components/checkout/checkout.component';
 import { AboutUsComponent } from './components/about-us/about-us.component';
 import { CheckouthomeComponent } from './components/checkouthome/checkouthome.component';
 import { CheckoutparcelComponent } from './components/checkoutparcel/checkoutparcel.component';
 import { OrdersComponent } from './components/orders/orders.component';
-
-import {MatExpansionModule} from '@angular/material/expansion';
+import { MatExpansionModule } from '@angular/material/expansion';
 
 const routes: Routes = [
   {path: 'orders', component: OrdersComponent},
@@ -49,6 +38,7 @@ const routes: Routes = [
   {path: '', redirectTo: '/plants', pathMatch: 'full'},
   {path: '**', redirectTo: '/plants', pathMatch: 'full'},
 ]
+
 @NgModule({
   declarations: [
     AppComponent,
@@ -81,4 +71,5 @@ const routes: Routes = [
   providers: [ProductService],
   bootstrap: [AppComponent]
 })
+
 export class AppModule { }

f6aff5b479f2afeb3b09acb026c20f815d6a7707
dainoras99 <ziukasdainoras@gmail.com>
2022-12-26 11:54:18 +0200

    encrypt/decrypt passwords implementation

--- plants-ecommerce/src/main/java/com/viko/plants/controller/RegistrationController.java
+++ plants-ecommerce/src/main/java/com/viko/plants/controller/RegistrationController.java
@@ -9,6 +9,7 @@ import lombok.AllArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.HashSet;
@@ -26,14 +27,16 @@ public class RegistrationController {
     @PostMapping("api/v1/login")
     public ResponseEntity<?> login (@RequestBody RegistrationRequest registrationRequest) {
         User user = repository.findByUsername(registrationRequest.getUsername());
-        if (user.getPassword().equals(registrationRequest.getPassword())) return ResponseEntity.ok(user);
+        BCryptPasswordEncoder bCryptPasswordEncoder = new BCryptPasswordEncoder();
+        if (bCryptPasswordEncoder.matches(registrationRequest.getPassword(), user.getPassword())) return ResponseEntity.ok(user);
         return (ResponseEntity<?>) ResponseEntity.internalServerError();
     }
 
     @PostMapping("api/v1/admin")
     public ResponseEntity<?> adminLogin (@RequestBody RegistrationRequest registrationRequest) {
         User user = repository.findByUsername(registrationRequest.getUsername());
-        if (user.getPassword().equals(registrationRequest.getPassword()) && user.getUserRole().equals(user.getUserRole().ADMIN)) return ResponseEntity.ok(user);
+        BCryptPasswordEncoder bCryptPasswordEncoder = new BCryptPasswordEncoder();
+        if (bCryptPasswordEncoder.matches(registrationRequest.getPassword(), user.getPassword()) && user.getUserRole().equals(user.getUserRole().ADMIN)) return ResponseEntity.ok(user);
         return (ResponseEntity<?>) ResponseEntity.internalServerError();
     }
 
@@ -41,6 +44,11 @@ public class RegistrationController {
     @PostMapping("api/v1/registration")
     public ResponseEntity<String> register(@RequestBody User user) {
         try {
+            //
+            BCryptPasswordEncoder bCryptPasswordEncoder = new BCryptPasswordEncoder();
+            String encryptedPassword = bCryptPasswordEncoder.encode(user.getPassword());
+            user.setPassword(encryptedPassword);
+            //
             List<User> users = repository.findAll();
             for (User tempUser: users) {
                 if (tempUser.getUsername().equals(user.getUsername())) {
